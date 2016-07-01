74e0a07a9d8e2936ed8c60d81d41f7f7cc7eb751
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-06-30 21:56:33 -0700

    add v1 prefix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var proxy = require('express-http-proxy');
 app.use('/v1/auth', proxy(process.env.AUTH_SERVICE_URL));
 app.use('/v1/recipes', proxy(process.env.RECIPES_SERVICE_URL, {
   forwardPath: function(req, res) {
-    return require('url').parse(req.url).path;
+    return '/v1/recipes' + require('url').parse(req.url).path;
   }
 }));
 

47bbe3e5bd7865229757341c3b7e20d3eb8f84ad
Nam Dao <nam.d.dao@gmail.com>
2016-07-07 19:56:20 -0700

    added filter routes to block non relevant user routes

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var app = require('express')();
 var proxy = require('express-http-proxy');
 
-app.use('/v1/auth', proxy(process.env.AUTH_SERVICE_URL));
+app.use('/v1/auth', proxy(process.env.AUTH_SERVICE_URL, {
+  filter: function(req,res) {
+    var endPoint = require('url').parse(req.url).path;
+    if (endPoint === '/verify' || endPoint === '/connect/gmail'
+    || endPoint === '/logout') {
+      return require('url').parse(req.url).path; 
+    }
+  },
+  forwardPath: function(req, res) {
+    return require('url').parse(req.url).path;
+  }
+}));    
 app.use('/v1/formulae', proxy(process.env.FORMULAE_SERVICE_URL, {
   forwardPath: function(req, res) {
     return '/v1/formulae' + require('url').parse(req.url).path;
@@ -17,4 +28,5 @@ app.use('/v1/twilio', proxy(process.env.FORMULAE_SERVICE_URL, {
     return '/v1/twilio' + require('url').parse(req.url).path;
   }
 }));
+
 app.listen(process.env.PORT || 3000);
